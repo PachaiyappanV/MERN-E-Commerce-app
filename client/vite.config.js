c2cd1ef47cd2d5c599c32ab6d311acd1f674e505
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-10 09:43:08 +0530

    feat(client): initial client setup with Vite, React, and shadcn
    - Set up the client-side application using Vite as the build tool for fast development and optimized performance.
    - Integrated React as the core framework for building dynamic user interfaces.
    - Added shadcn for a customizable component library to enhance the design and usability of the application.
    - Configured initial project structure to ensure modularity and scalability.
    - Installed essential development dependencies and tools for a smooth development experience.

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+// Define __dirname using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+    },
+  },
+});
