2cba9278b74a4f37694a0dfa2de1eddce87224a2
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-13 13:11:51 +0530

    chore(client): add initial structure for admin and shopping pages with layouts
    - Created new layout components: `AdminLayout` for the admin section and `ShoppingLayout` for the shopping experience.
    - Added placeholder pages for the admin section: `AdminDashboard`, `AdminFeatures`, `AdminOrders`, and `AdminProducts`.
    - Added placeholder pages for the shopping section: `ShoppingHome`, `ShoppingListing`, `ShoppingCheckout`, and `ShoppingAccount`.
    - Updated the `App` component to include new routes for admin and shopping sections using `react-router-dom`.
    - Structured routes for better scalability and maintainability, preparing for future development.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./components/layout";
+import AdminLayout from "./components/admin/layout";
+import AuthLayout from "./components/auth/layout";
+import ShoppingLayout from "./components/shopping/layout";
+import AdminDashboard from "./pages/admin/dashboard";
+import AdminFeatures from "./pages/admin/features";
+import AdminOrders from "./pages/admin/orders";
+import AdminProducts from "./pages/admin/products";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
+import ShoppingAccount from "./pages/shopping/account";
+import ShoppingCheckout from "./pages/shopping/checkout";
+import ShoppingHome from "./pages/shopping/home";
+import ShoppingListing from "./pages/shopping/listing";
 
 function App() {
   return (
@@ -10,6 +20,18 @@ function App() {
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
         </Route>
       </Routes>
     </div>
