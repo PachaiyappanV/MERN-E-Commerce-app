fc0ce50d8a3de7a7e12ab97395ec23af1365fee3
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-13 13:47:07 +0530

    feat(client): implement CheckAuth component for route protection
    - Created `CheckAuth` component to handle route protection based on authentication status and user role.
    - Implemented logic to redirect unauthenticated users to the login page when accessing protected routes.
    - Redirected users based on their roles (admin or regular user) to appropriate sections upon authentication.
    - Added conditional checks to prevent authenticated users from accessing login or registration pages.
    - Ensured unauthorized access to admin routes redirects non-admin users to an "unauthorized" page.
    - Enhanced security and user experience by managing route access dynamically based on authentication.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AdminLayout from "./components/admin/layout";
 import AuthLayout from "./components/auth/layout";
+import CheckAuth from "./components/common/check-auth";
 import ShoppingLayout from "./components/shopping/layout";
 import AdminDashboard from "./pages/admin/dashboard";
 import AdminFeatures from "./pages/admin/features";
@@ -17,17 +18,38 @@ function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
